b5ec62abc66104b89cdd3be0a10fe6dcceb5b8fd
Dan Gold <tentativechaos@gmail.com>
2020-05-25 12:33:21 -0700

    Created test version of email feature.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,26 +1,47 @@
-import Email from "./smtp";
+function loadEmailSubroutine() {
+  return new Promise((resolve, reject) => {
+    let scriptTag = document.createElement("script");
+    scriptTag.type = "text/javascript";
+    scriptTag.src = "./assets/scripts/smtp.js";
 
-// Email send
-function sendEmail(email, subject, body) {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "bcultjoinerdaemon",
-    Password: "bcult5ever",
-    To: email,
-    From: "bcultjoinerdaemon@gmail.com",
-    Subject: subject,
-    Body: body,
-  }).then((message) => console.log(message));
+    scriptTag.onload = () => resolve(true);
+    scriptTag.onerror = () => reject(error);
+    document.getElementsByTagName("head")[0].appendChild(scriptTag);
+  });
 }
 
-const joinButton = document.getElementsByClassName("join-button");
+loadEmailSubroutine().then(main);
 
-// Just for testing
-const testData = {
-  email: "tentativechaos@gmail.com",
-  subject: "Test Email from BCULT",
-  body: "This is the test email from BCULT. Testing, testing, testing.",
-};
+function main() {
+  function sendEmail(email, subject, body) {
+    Email.send({
+      Host: "smtp.gmail.com",
+      Username: "bcultjoinerdaemon",
+      Password: "bcult5ever",
+      To: email,
+      From: "bcultjoinerdaemon@gmail.com",
+      Subject: subject,
+      Body: body,
+    }).then((message) => console.log(message));
+  }
 
-// Click even for join buttons
-joinButton.addEventListener("click", sendEmail(...testData));
+  let joinButtons = document.getElementsByClassName("join-button");
+
+  // Just for testing
+  const testData = {
+    email: "tentativechaos@gmail.com",
+    subject: "Test Email from BCULT",
+    body: "This is the test email from BCULT. Testing, testing, testing.",
+  };
+
+  // Click even for join buttons
+  for (let button of joinButtons) {
+    button.addEventListener("click", () => {
+      sendEmail(
+        "tentativecahos@gmail.com",
+        "Test Email from BCULT",
+        "This is the test email from BCULT. Testing, testing, testing."
+      );
+    });
+  }
+}
